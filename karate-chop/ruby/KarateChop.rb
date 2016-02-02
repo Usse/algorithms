c4037eef7ce910678f1523d639f9d89161bfd61a
Andrea Usseglio <darkusse@gmail.com>
2016-02-02 14:56:00 +0000

    ruby iterative vesion

--- karate-chop/ruby/KarateChop.rb
+++ karate-chop/ruby/KarateChop.rb
@@ -1,7 +1,21 @@
 class KarateChop
 
   def iterativeChop(elem,array)
-    -1
+    minIndex = 0
+    maxIndex = array.length - 1
+
+    while(minIndex<=maxIndex) do
+      pivot = (minIndex + maxIndex)/2
+      if elem > array[pivot]
+        minIndex = pivot+1
+      elsif elem < array[pivot]
+        maxIndex = pivot-1
+      else
+        return pivot
+      end
+    end
+
+    return -1
   end
-  
+
 end
